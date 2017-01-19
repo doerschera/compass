00c9918981cce28038867e03aeabcab490d00bd3
doerschera <doerschera@gmail.com>
2017-01-19 13:36:29 -0600

    remove material ui, add react grid, set up style folder

--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import customTheme from 'material-ui/styles/baseThemes/customTheme';
+import { Card, CardHeader } from 'material-ui/card';
+
 class App extends React.Component {
 
   render () {
-    return <h1>Hello World</h1>
+    return (
+      <h1>Hello World</h1>
+    )
   }
 }
 
